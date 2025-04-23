8058b143a16b77d7d8d550175ef309548c27ac04
Gaurav23p24 <gpatel23@charlotte.edu>
2025-04-22 20:09:28 -0400

    Commit 1

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'MedBots - Medical Chatbot Generative AI Project',
+    version= '0.0.0',
+    author= 'Rishikesh Reddy Regatte',
+    author_email= 'rregatt1@charlotte.edu',
+    packages= find_packages(),
+    install_requires = []
+
+)
